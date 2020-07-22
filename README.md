7bbb4857ecdaada295782e96ea28676a86c4fb67
hongmei <duhongmei@protonmail.com>
2020-07-22 15:10:46 +0800

    Clean up and Restructure Cleaned up comments in app.js. Add and corrected site.webmanifest properties. Updated readme.md Restructured folder. Moved favicons to root directory.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# ✅ Todo App
+<div align="center">
+  <h1>✅  Todo App</h1>
+
 
 [![Website](https://img.shields.io/website?down_color=red&down_message=offline&label=Demo&style=for-the-badge&up_color=b&up_message=online&url=https%3A%2F%2Fhongmei-codes.github.io%2Fvanilla_todo%2F)](https://hongmei-codes.github.io/vanilla_todo/)
+
+</div>
 
 This is a simple todo web app built with vanilla JavaScript. No frameworks were used to create this app. The UI is simple and easily understood. Read on to find out more about its features. Feel free to try out the [demo](https://hongmei-codes.github.io/vanilla_todo/).
 
@@ -30,4 +34,10 @@ Remove todo item from list by clicking the trash can icon.
 
 Filter your todo items by their completion status.
 
----
+
+# Technologies used
+- [x] HTML
+- [x] CSS
+- [x] Vanilla Javascript
+
+
--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-// DOM Elements
-// @@shortcut: shift, alt, down arrow -> dulicate current line
+// ==================================SELECTORS==================================
 const date = document.getElementById("date");
 const todoInput = document.querySelector(".todo-input");
 const addButton = document.querySelector(".add-button");
 const todos = document.querySelector(".todos");
 const filterOption = document.querySelector(".filter-todo");
+// ==================================SELECTORS==================================
 
-// EVENT LISTENERS
+
+// ==================================EVENT LISTENERS==================================
 document.addEventListener("DOMContentLoaded", showStorage); // listen to page load
-addButton.addEventListener("click", addTodo); // listen to add todo button
-todos.addEventListener("click", doneDelete); // listen to todo item
+addButton.addEventListener("click", addTodo); // listen to add todo button click
+todos.addEventListener("click", doneDelete); // listen to todo item div
 filterOption.addEventListener("change", filterTodo); // listen to filter todo select field
+// ==================================EVENT LISTENERS==================================
 
-// FUNCTIONS
+
+// ==================================FUNCTIONS==================================
 
 // show date
 let today = new Date();
 let format = { weekday: "short", month: "short", day: "numeric" };
 date.innerHTML = today.toLocaleDateString("en-US", format);
 
-// function to add todo items to todo items section
+// function generate html for todo item
 function getHTML(itemTitle, done) {
   const doneItem = `<div class="item completed">
     <li class="title">${itemTitle}</li>
@@ -34,33 +37,11 @@ function getHTML(itemTitle, done) {
   </div>`;
 
   return (done ? doneItem : notDoneItem);
-  // create div to contain all elements of todo item
-  // const itemDiv = document.createElement("div"); // create <div> that contains todo item
-  // itemDiv.classList.add("item"); // give item div class attribute of 'item' to use for styling
-
-  // const item = document.createElement("li"); // <li> that contains todo title
-  // item.classList.add("title"); // give li tag class attribute of title
-  // item.innerText = itemTitle; // test value to see if function works
-  // itemDiv.appendChild(item); // add <li class="title"> as child of <div class="item">
-
-  // const doneButton = document.createElement("button"); // create <button> for mark done
-  // doneButton.classList.add("done-button"); // add class attribute for styling
-  // doneButton.innerHTML = '<i class="fas fa-check"></i>'; // give button value of checkbox icon
-  // itemDiv.appendChild(doneButton); // add button to <div> parent element
-
-  // const deleteButton = document.createElement("button"); // create <button> for deleting todo item
-  // deleteButton.classList.add("delete-button"); // add class attribute for styling
-  // deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; // give button value of trash icon
-  // itemDiv.appendChild(deleteButton); // add button to <div> parent element
-
-  // // add completed item div to todos <ul>
-  // todos.appendChild(itemDiv);
 }
 
 // function to add todo item elements to todo items section
 function addTodo(event) {
-  // console.log("addTodo executed 👍🏼") // see if event listener works
-  event.preventDefault(); // prevent browser from automatically submitting so you can see the message in console
+  event.preventDefault(); // prevent automatic default refresh
 
   const itemTitle = todoInput.value;
 
@@ -68,7 +49,7 @@ function addTodo(event) {
     // input value is none, show alert
     return alert("Add todo cannot be blank!");
   } else {
-    // valid input value, add show item in todo item selection
+    // valid input value, add show item in todo item section
     const text = getHTML(itemTitle, false);
     todos.insertAdjacentHTML("afterbegin", text);
   }
@@ -83,32 +64,26 @@ function addTodo(event) {
 // function to mark todo item as done / delete todo item
 function doneDelete(e) {
   const button = e.target;
-  const item = button.parentElement;
+  const item = button.parentElement; // item div element
 
+  // mark done
   if (button.classList[0] == "done-button") {
-    // item div element
     item.classList.toggle("completed"); // add completed to class name if not there
-    markDone(item); // update in local storage
+    markDone(item); // update state in local storage
   }
 
+  // delete
   if (button.classList[0] == "delete-button") {
     item.remove(); // removes the item div element
-    remove(item); // update in local storage
+    remove(item); // update deletion in local storage
   }
 }
 
 // function to filter todo items by class attribute
 function filterTodo(e) {
   const items = todos.children;
-  // .childNodes gets an element's child nodes
-  // e.g. <head>, <body> are child node of <html> element
-  // the aim is to find all item <div>s and show them accordingly
-  // completed items are in <div class="item">
-  // uncompleted items are in <div class="item completed">
 
-  const itemsArray = Array.from(items); // change items to array to loop through below
-
-  // console.log(itemsArray) // see if event listener works
+  const itemsArray = Array.from(items); // change items to array to loop through
 
   // find which option (all/done/not done) is selected
   itemsArray.forEach(function (item) {
@@ -119,7 +94,7 @@ function filterTodo(e) {
         item.style.display = "flex";
         break;
       case "done":
-        // if class contains completed, item is done
+        // if class attribute contains completed, item is done
         // show in the items section
         // otherwise, do not show
         if (item.classList.contains("completed")) {
@@ -166,6 +141,7 @@ function save(item) {
     item: item,
     done: false,
   }); // add item to array
+
   localStorage.setItem("vanilla_todos", JSON.stringify(currentStorage)); // add array to local storage
 }
 
@@ -181,7 +157,7 @@ function remove(item) {
   localStorage.setItem("vanilla_todos", JSON.stringify(newStorage)); // add array to local storage
 }
 
-// function to save done status in local storage
+// function to update done status in local storage
 function markDone(item) {
   let currentStorage = checkStorage();
   const title = item.children[0].innerText;
@@ -201,6 +177,7 @@ function markDone(item) {
 
   localStorage.setItem("vanilla_todos", JSON.stringify(newStorage)); // add array to local storage
 }
+
 // function to show items in storage upon page load
 function showStorage() {
   let currentStorage = checkStorage();
@@ -211,3 +188,4 @@ function showStorage() {
     todos.insertAdjacentHTML("afterbegin", text);
   });
 }
+// ==================================FUNCTIONS==================================
